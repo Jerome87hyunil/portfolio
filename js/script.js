92dd5435df64830a1c1091c7f717e0394404529d
a <a@aui-MacBookPro.local>
2025-07-04 18:35:44 +0900

    design

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 // Custom scripts for portfolio 
+
+document.addEventListener('DOMContentLoaded', function () {
+    var typedOptions = {
+        strings: ['풀스택 개발자', '인디해커'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    };
+    var typed = new Typed('#typed-text', typedOptions);
+
+    VANTA.WAVES({
+        el: "#home",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0x1a2330,
+        shininess: 25.00,
+        waveHeight: 15.00,
+        waveSpeed: 0.75,
+        zoom: 0.75
+    });
+}); 
